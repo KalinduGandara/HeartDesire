92973d603c9dcbdb80fec10c241aa5dd3827d699
KalinduGandara <kalindugandara16@gmail.com>
2021-11-04 00:38:13 +0530

    remove django heroku

--- HeartsDesire/settings.py
+++ HeartsDesire/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+# import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -150,4 +150,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login'
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
