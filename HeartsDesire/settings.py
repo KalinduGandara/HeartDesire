2c1c193ff6d241acff878010d1eab3085ad5b973
KalinduGandara <kalindugandara16@gmail.com>
2021-11-04 00:43:10 +0530

    edit req

--- HeartsDesire/settings.py
+++ HeartsDesire/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-# import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -149,5 +148,3 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login'
-# Activate Django-Heroku.
-# django_heroku.settings(locals())
